f204cd58d1b3ffd74d2c21d59d5a652c2bce8759
Prakash Adekkanattu <pra2008@med.cornell.edu>
2019-04-09 11:33:46 -0400

    Modified to run in batch mode.

--- citation_impact_lite/custom_medline_fetch.php
+++ citation_impact_lite/custom_medline_fetch.php
@@ -4,6 +4,7 @@ include_once(__DIR__ . "/library/portable_utf8.php");
 $mysqli = getDatabaseConnection();
 require_file('data.php');
 $objData = new Data($mysqli);
+
 $year = '';
 if (count($argv) == 4 ) {
     if (isset($argv[1])) {
@@ -27,7 +28,7 @@ if (count($argv) == 4 ) {
 }else {
     exit;
 }
-$limit = 20;
+$limit = 200;
 
 $pub_types = array();
 
@@ -56,31 +57,76 @@ foreach ($pub_types as $type) {
             $ids = array();
             $ids = $objData->get_category_journal_ids($key);
 
+            $pubmed_efetch_grand_results = array();
+
             if (!empty($ids)) {
-                $term = $objData->construct_query($year, $type, $ids);
+                $chunks = array_chunk($ids, 100, true);
+
+                foreach ($chunks as $chunk) {
+                    $start = microtime();
+
+                    $previous_efetch_results = array();
+                    foreach($pubmed_efetch_grand_results as $k=>$v){
+                        $previous_efetch_results[$k] = $v;
+                    }
+
+                    $term = $objData->construct_query($year, $type, $chunk);
+
+                    $pmids = array();
+                    $pmids = $objAPI->query($term);
+
+                    $result_count = count($pmids);
+
+                    // Get return count
+                    $random_count = ($result_count >= $limit)? $limit : $result_count;
+
+                    // get xml for all articles with  a random 200 pmids from this list
+                    $chunk_efetch_results = array();
+                    $chunk_efetch_results = $objAPI->pubmed_random_efetch($pmids, $random_count);
+
+                    // merge with previous results
+                    $merged_results = array();
+                    if (count($chunk_efetch_results) > 0) {
+                        if (count($previous_efetch_results) > 0){
+                            $merged_results = array_merge($previous_efetch_results, $chunk_efetch_results);
+                        }else {
+                            $merged_results = $chunk_efetch_results;
+                        }
+                    }else {
+                        if (count($previous_efetch_results) > 0){
+                            $merged_results = $previous_efetch_results;
+                        }
+                    }
+
+                    $pubmed_efetch_grand_results = array();
+                    foreach($merged_results as $k=>$v){
+                        $pubmed_efetch_grand_results[$k] = $v;
+                    }
+
+                    $end = microtime();
+                    if(($end - $start) < 333000 ) {
+                        usleep(333000);
+                    }
+
+                }
+                if (!empty($pubmed_efetch_grand_results)) {
+
+                    $pubmed_efetch_200_rand_results = array();
+                    if(count($pubmed_efetch_grand_results) > $limit) {
+                        shuffle($pubmed_efetch_grand_results); // randomize order of array items
+                        $pubmed_efetch_200_rand_results = array_slice($pubmed_efetch_grand_results, 0, $limit);
+                    }else {
+                        $pubmed_efetch_200_rand_results = $pubmed_efetch_grand_results;
+                    }
+                    // echo "Count:" . count($pubmed_efetch_200_rand_results)."\n";
+
+                    // populate local database with the result
+                    // $objData->populate_data( $year, $type, $key, $pubmed_efetch_results );
+                    $objData->populate_multi_data($year, $type, $key, $pubmed_efetch_200_rand_results);
+
+                    echo "Processed, Year:" . $year . ", Type:" . $type . ", Category:" . $key . "\n";
+                }
             }
-
-            // $term = $objData->construct_query(2008, "Review", $ids);
-
-            // Get Medline XML with PMID list for this search term
-            $pmids = array();
-            $pmids = $objAPI->query($term);
-
-            $result_count = count($pmids);
-
-            // Get return count
-            $random_count = ($result_count >= 200) ? 200 : $result_count;
-
-            // get xml for all articles with  a random 200 pmids from this list
-            $pubmed_efetch_results = array();
-            $pubmed_efetch_results = $objAPI->pubmed_random_efetch($pmids, $random_count);
-
-            // populate local database with the result
-            // $objData->populate_data( $year, $type, $key, $pubmed_efetch_results );
-            $objData->populate_multi_data($year, $type, $key, $pubmed_efetch_results);
-
-            echo "Processed, Year:" . $year . ", Type:" . $type . ", Category:" . $key . "\n";
-
         }
     }
 }
@@ -89,4 +135,3 @@ $count++;
 
 
 ?>
-
--- citation_impact_lite/medline_fetch.php
+++ citation_impact_lite/medline_fetch.php
@@ -12,7 +12,7 @@ $mysqli = getDatabaseConnection();
 require_file('data.php');
 $objData = new Data($mysqli);
 
-$limit = 20;
+$limit = 200;
 
 $pub_years = array(
     '2003',
@@ -35,41 +35,80 @@ foreach ($pub_years as $year) {
         if (count($categories) > 0) {
             foreach ($categories as $key => $val) {
 
-                $start = microtime();
-
                 // construct queries
                 $ids = array();
                 $ids = $objData->get_category_journal_ids($key);
 
+                $pubmed_efetch_grand_results = array();
+
                 if(!empty($ids)){
-                    $term = $objData->construct_query($year, $type, $ids);
-                }
 
-                // $term = $objData->construct_query(2008, "Review", $ids);
+                    $chunks = array_chunk($ids, 100, true);
 
-                // Get Medline XML with PMID list for this search term
-                $pmids = array();
-                $pmids = $objAPI->query($term);
+                    foreach ($chunks as $chunk) {
+                        $start = microtime();
 
-                $result_count = count($pmids);
+                        $previous_efetch_results = array();
+                        foreach($pubmed_efetch_grand_results as $k=>$v){
+                            $previous_efetch_results[$k] = $v;
+                        }
 
-                // Get return count
-                $random_count = ($result_count >= $limit)? $limit : $result_count;
+                        $term = $objData->construct_query($year, $type, $chunk);
 
-                // get xml for all articles with  a random 200 pmids from this list
-                $pubmed_efetch_results = array();
-                $pubmed_efetch_results = $objAPI->pubmed_random_efetch($pmids, $random_count);
+                        $pmids = array();
+                        $pmids = $objAPI->query($term);
 
-                // populate local database with the result
-                // $objData->populate_data( $year, $type, $key, $pubmed_efetch_results );
-                $objData->populate_multi_data( $year, $type, $key, $pubmed_efetch_results );
+                        $result_count = count($pmids);
 
-                echo "Processed, Year:".$year. ", Type:". $type. ", Category:". $key . "\n";
+                        // Get return count
+                        $random_count = ($result_count >= $limit)? $limit : $result_count;
 
-                $end = microtime();
+                        // get xml for all articles with  a random 200 pmids from this list
+                        $chunk_efetch_results = array();
+                        $chunk_efetch_results = $objAPI->pubmed_random_efetch($pmids, $random_count);
 
-                if(($end - $start) < 333000 ) {
-                    usleep(333000);
+                        // merge with previous results
+                        $merged_results = array();
+                        if (count($chunk_efetch_results) > 0) {
+                            if (count($previous_efetch_results) > 0){
+                                $merged_results = array_merge($previous_efetch_results, $chunk_efetch_results);
+                            }else {
+                                $merged_results = $chunk_efetch_results;
+                            }
+                        }else {
+                            if (count($previous_efetch_results) > 0){
+                                $merged_results = $previous_efetch_results;
+                            }
+                        }
+
+                        $pubmed_efetch_grand_results = array();
+                        foreach($merged_results as $k=>$v){
+                            $pubmed_efetch_grand_results[$k] = $v;
+                        }
+
+                        $end = microtime();
+                        if(($end - $start) < 333000 ) {
+                            usleep(333000);
+                        }
+
+                    }
+                    if (!empty($pubmed_efetch_grand_results)) {
+
+                        $pubmed_efetch_200_rand_results = array();
+                        if(count($pubmed_efetch_grand_results) > $limit) {
+                            shuffle($pubmed_efetch_grand_results); // randomize order of array items
+                            $pubmed_efetch_200_rand_results = array_slice($pubmed_efetch_grand_results, 0, $limit);
+                        }else {
+                            $pubmed_efetch_200_rand_results = $pubmed_efetch_grand_results;
+                        }
+                        // echo "Count:" . count($pubmed_efetch_200_rand_results)."\n";
+
+                        // populate local database with the result
+                        // $objData->populate_data( $year, $type, $key, $pubmed_efetch_results );
+                        $objData->populate_multi_data($year, $type, $key, $pubmed_efetch_200_rand_results);
+
+                        echo "Processed, Year:" . $year . ", Type:" . $type . ", Category:" . $key . "\n";
+                    }
                 }
             }
         }
@@ -77,8 +116,6 @@ foreach ($pub_years as $year) {
 
     $count++;
 
-
 }
 
 ?>
-
